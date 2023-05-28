b519150cc269437a3d325260961d54239e2f0c55
Don Dang <doncdang@hotmail.com>
2023-05-28 14:14:02 -0700

    Start working on lesson08 assignment class-based view

--- blogging/views.py
+++ blogging/views.py
@@ -11,54 +11,52 @@ Resources:
 Introduction to class-based views
 - https://docs.djangoproject.com/en/2.1/topics/class-based-views/intro/
 
-
-
-
 Notes:
 https://canvas.uw.edu/courses/1616579/pages/lesson-07-content?module_item_id=17606304
   In the Starting Django tutorial, you learned about Django urlconfs. We used our project urlconf to hook the Django admin into our project
   and we want to do the same thing for our new app. In general, an app that serves any sort of views should contain its own urlconf.
   The project urlconf should mainly include these where possible.
 
-
+Date        Developer       Activities
+5/28/23     Don D.          Remove def stub_view()
+5/28/23     Don D.          import class-based views: ListView
 """
 from django.template import loader
 from blogging.models import Post, Category
 from django.shortcuts import HttpResponse, Http404
 from django.shortcuts import render
 
-
-# Create your views here.
-def stub_view(request, *args, **kwargs):
-    """
-    a view:
-    - code that builds a page that you can see
-    - can be defined as a callable that takes a request and returns a response.
-    we'll use the 'stub view' we've created so we can concentrate on the url routing
-    """
-    body = "Stub View\n\n"
-    if args:
-        body += "Args: \n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs: \n"
-        body += "\n".join(["\t%s: %s" % a for a in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")  # return body with mime type
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)  # create an instance from the blogging/models's Post
-    posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html') # get a template from the loader
-    context = {'posts': posts} # build a context
-    return render(request, 'blogging/list.html', context)
+# Create your class-based views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
 
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class PostListViewDetail(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+
+    # def get_queryset(self):
+
+
+#
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)  # create an instance from the blogging/models's Post
+#     posts = published.order_by('-published_date')
+#     template = loader.get_template('blogging/list.html') # get a template from the loader
+#     context = {'posts': posts} # build a context
+#     return render(request, 'blogging/list.html', context)
+#
+#
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
